44623d9e81b3f667663dc309f6cef1ef424f51ec
Gregory <loveyourselfgregory@gmail.com>
2026-07-28 13:19:14 +0300

    returned the original page size value

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import { resolve } from '$app/paths';
 
-export const PAGE_SIZE = 2;
+export const PAGE_SIZE = 20;
 export const CACHE_TTL = 900; // 15 minutes, in seconds
 export const CACHE_TTL_LONG = 604800; // 7 days, in seconds
 
